c42e90f858db503c19162bdbc4c1e353aec8ea11
Falasefemi2 <femifalase228@gmail.com>
2024-08-18 20:07:35 +0100

    clerk auth dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from '@clerk/themes'
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,23 +13,38 @@ export const metadata: Metadata = {
   description: "Demo game store made with NEXTJS",
 };
 
+const clerkAppearance = {
+  baseTheme: dark,
+  variables: {
+    colorPrimary: "white",
+    colorText: "white",
+  },
+  elements: {
+    formButtonPrimary: {
+      color: "black",
+    },
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider appearance={clerkAppearance}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
