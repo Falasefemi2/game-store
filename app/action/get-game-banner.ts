6b40fcd0e4dcd9d391a41b2d5b417150b9579967
Falasefemi2 <femifalase228@gmail.com>
2024-08-25 13:03:16 +0100

    get games banner

--- app/action/get-game-banner.ts
+++ app/action/get-game-banner.ts
@@ -1,5 +1,24 @@
 /** @format */
 
-// "use server";
+"use server";
 
-// export async function getThumbnails() {}
+import { db } from "@/app/db";
+import { games } from "@/app/db/schema";
+
+export async function getSevenGameThumbnails() {
+  try {
+    const sevenGames = await db
+      .select({
+        id: games.id,
+        title: games.title,
+        thumbnail: games.thumbnail,
+      })
+      .from(games)
+      .limit(7);
+
+    return sevenGames;
+  } catch (error) {
+    console.error("Error fetching game thumbnails:", error);
+    throw new Error("Failed to fetch game thumbnails");
+  }
+}
